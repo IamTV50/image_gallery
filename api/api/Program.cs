a72998ed92dd59c2f82d6d3696385beecdc54d1c
IamTV50 <95366061+IamTV50@users.noreply.github.com>
2024-03-01 12:54:56 +0100

    updated CORS rules

--- api/api/Program.cs
+++ api/api/Program.cs
@@ -10,6 +10,11 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+// Enable CORS
+app.UseCors(builder => builder.WithOrigins(
+	"http://localhost:3000/"
+	).AllowAnyMethod().AllowAnyHeader());
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
 	app.UseSwagger();
